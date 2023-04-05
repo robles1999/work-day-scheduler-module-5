072d4f593c4895c204d344e0dc226475b9f5e1b5
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-04-05 14:54:00 -0400

    updated file structure.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Calendar App
+
+![Screenshot](work-day-scheduler-module-5/Assets/scheduler-screenshot_2023-04-05.jpg)
+
+This is a simple calendar app built with jQuery and Moment.js that allows users to save events for each hour of the day. The app displays time blocks for standard business hours and color codes each block to indicate whether it is in the past, present, or future. Users can click on a time block to enter an event and save it to local storage. The saved events persist even after refreshing the page.
+
+## Usage
+
+When you open the app, you'll see a calendar that displays the current date at the top of the page. The calendar is divided into time blocks for standard business hours. Each time block is color coded to indicate whether it is in the past, present, or future.
+
+To enter an event for a specific time block, simply click on the block and type in your event. Then click the save button on the right side of the block to save the event to local storage.
+
+If you refresh the page, the saved events will still be there.
+
+## Credits
+
+This app was built using the following technologies:
+
+- HTML
+- CSS
+- jQuery
+- JavaScript
+- Dayjs
+- Bootstrap
+
---
+++ script.js
@@ -0,0 +1,66 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(document).ready(() => {
+  // Load previous local storage data in an array of Objects format.
+  const calendarData = JSON.parse(localStorage.getItem("calendar") || "[]");
+  $(function () {
+    // Event listener for click events on the save button.
+    $(".fa-save").on("click", function (e) {
+      // Find the parent div element of the clicked element
+      const parent = $(this).closest('.time-block');
+
+      // Get the ID of the parent div element
+      const id = parent.attr('id');
+
+      // Get the text inside the corresponding textarea
+      const text = parent.find('textarea').val();
+
+      // Save calendar event to local storage
+      const data = {};
+      data.time = id;
+      data.task = text;
+      calendarData.push(data);
+      localStorage.setItem("calendar", JSON.stringify(calendarData))
+    })
+    
+    // Code to apply the past, present, or future class to each time
+    function checkTime() {
+      // Get hour in military time
+      var now = Number(dayjs().format("H"));
+      let previousTime = 0;
+
+      // Only change the time block background if the time
+      // is between the working hours and if the time changed
+      if (now > 8 && now < 18) {
+        if (now > previousTime) {
+          previousTime = now;
+           $(".container-fluid").find(".time-block").each(function (index) {
+            const timeSlotId = Number($(this).attr("id").split("-")[1]);
+
+            if (timeSlotId < now){
+              $(this).removeClass().addClass("row time-block past");
+            } else if (timeSlotId === now) {
+              $(this).removeClass().addClass("row time-block present");
+            } else if (timeSlotId > now) {
+              $(this).removeClass().addClass("row time-block future");
+            }
+          })
+        }
+      }
+    }
+    // Call the checkTime function every second
+    setInterval(checkTime, 1000);
+
+    // Get any user input that was saved in localStorage and set
+    // the values of the corresponding textarea elements. 
+    calendarData.forEach(e => {
+      const { time, task } = e
+      $("#" + time).children(1).val(task);
+    })
+
+    // Display the current date in the header of the page.
+    const todaysDate = dayjs().format("dddd, MMMM D, YYYY");
+    $("#currentDay").text(todaysDate)
+  });
+});
